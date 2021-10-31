82c8d6c6127a46d881b3510c68edd95401be522c
moostofa <mmdrahinz96@gmail.com>
2021-10-31 20:20:54 +1100

    implemented working search route

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,10 +1,11 @@
-from django.http.response import HttpResponse, HttpResponsePermanentRedirect
+from logging import error
+from django.http.response import HttpResponse
 from django.shortcuts import render
+from django import forms
 
 from markdown2 import markdown
 
 from .util import list_entries, save_entry, get_entry
-
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
@@ -17,17 +18,25 @@ def entry(request, entry):
     #check if wiki entry exists
     entries = [element.upper() for element in list_entries()]
     if entry.upper() not in entries:
-        return render(request,"encyclopedia/error.html")
+        return render(request,"encyclopedia/error.html", {
+            "message": "Wiki page for the chosen entry does not exist"
+        })
 
-    #render the remplate and pass in markdown text
+    #render the remplate and pass in the markdown text
     return render(request, "encyclopedia/entry.html", {
         "title": entry,
         "entry": markdown(get_entry(entry))
     })    
 
+#TODO: search entry
 def search(request):
-    #TODO: search entry
-    return
+    if request.method == "POST":
+        q = request.POST["q"]
+        return entry(request, q)
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "message": "Something went wrong, search form should not have method of GET"
+        })
 
 def new(request):
     #TODO: create new entry
